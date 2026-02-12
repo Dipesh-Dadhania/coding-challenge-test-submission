0ef0696fda3b93f21ca79938b5beaa31afd9f9c3
Dipesh Dadhania <dipeshdadhania4@gmail.com>
2026-02-13 00:27:24 +1100

    Add Unit Test Cases

--- pages/api/getAddresses.ts
+++ pages/api/getAddresses.ts
@@ -3,71 +3,71 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import generateMockAddresses from "../../src/utils/generateMockAddresses";
 
 export default async function handle(
-    req: NextApiRequest,
-    res: NextApiResponse,
+  req: NextApiRequest,
+  res: NextApiResponse,
 ) {
-    const {
-        query: { postcode, streetnumber },
-    } = req;
+  const {
+    query: { postcode, streetnumber },
+  } = req;
 
-    if (!postcode || !streetnumber) {
-        return res.status(400).send({
-            status: "error",
-            // DO NOT MODIFY MSG - used for grading
-            errormessage: "Postcode and street number fields mandatory!",
-        });
+  if (!postcode || !streetnumber) {
+    return res.status(400).json({
+      status: "error",
+      // DO NOT MODIFY MSG - used for grading
+      errormessage: "Postcode and street number fields mandatory!",
+    });
+  }
+
+  if (postcode.length < 4) {
+    return res.status(400).json({
+      status: "error",
+      // DO NOT MODIFY MSG - used for grading
+      errormessage: "Postcode must be at least 4 digits!",
+    });
+  }
+
+  /** Implementation of validation logic to ensure input value
+   *  is all digits and non negative
+   */
+  const isStrictlyNumeric = (value: string) => {
+    return /^\d+$/.test(value);
+  };
+
+  // Validate postcode and street number fields
+  const fieldsToValidate = [
+    { value: postcode as string, name: "Postcode" },
+    { value: streetnumber as string, name: "Street Number" },
+  ];
+
+  for (const field of fieldsToValidate) {
+    if (!isStrictlyNumeric(field.value)) {
+      return res.status(400).json({
+        status: "error",
+        errormessage: `${field.name} must be all digits and non negative!`,
+      });
     }
+  }
 
-    if (postcode.length < 4) {
-        return res.status(400).send({
-            status: "error",
-            // DO NOT MODIFY MSG - used for grading
-            errormessage: "Postcode must be at least 4 digits!",
-        });
-    }
-
-    /** Implementation of validation logic to ensure input value
-     *  is all digits and non negative
-     */
-    const isStrictlyNumeric = (value: string) => {
-        return /^\d+$/.test(value);
+  const mockAddresses = generateMockAddresses(
+    postcode as string,
+    streetnumber as string,
+  );
+  if (mockAddresses) {
+    const timeout = (ms: number) => {
+      return new Promise((resolve) => setTimeout(resolve, ms));
     };
 
-    // Validate postcode and street number fields
-    const fieldsToValidate = [
-        { value: postcode as string, name: "Postcode" },
-        { value: streetnumber as string, name: "Street Number" },
-    ];
-
-    for (const field of fieldsToValidate) {
-        if (!isStrictlyNumeric(field.value)) {
-            return res.status(400).send({
-                status: "error",
-                errormessage: `${field.name} must be all digits and non negative!`,
-            });
-        }
-    }
-
-    const mockAddresses = generateMockAddresses(
-        postcode as string,
-        streetnumber as string,
-    );
-    if (mockAddresses) {
-        const timeout = (ms: number) => {
-            return new Promise((resolve) => setTimeout(resolve, ms));
-        };
-
-        // delay the response by 500ms - for loading status check
-        await timeout(500);
-        return res.status(200).json({
-            status: "ok",
-            details: mockAddresses,
-        });
-    }
-
-    return res.status(404).json({
-        status: "error",
-        // DO NOT MODIFY MSG - used for grading
-        errormessage: "No results found!",
+    // delay the response by 500ms - for loading status check
+    await timeout(500);
+    return res.status(200).json({
+      status: "ok",
+      details: mockAddresses,
     });
+  }
+
+  return res.status(404).json({
+    status: "error",
+    // DO NOT MODIFY MSG - used for grading
+    errormessage: "No results found!",
+  });
 }
